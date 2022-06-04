a73a705819fec06758702cf0a955d40874704ef9
albert patterson <apatterson189@gmail.com>
2022-06-04 11:45:26 -0500

    add license notification

--- readme/write.js
+++ readme/write.js
@@ -6,6 +6,12 @@
  * @see [npm]{@link https://www.npmjs.com/~apatterson189}
  * @see [Youtube]{@link https://www.youtube.com/channel/UCrECEffgWKBMCvn5tar9bYw}
  * @see [Medium]{@link https://medium.com/@albert.patterson.code}
+ *
+ * Free software under the GPLv3 licence. Permissions of this strong copyleft
+ * license are conditioned on making available complete source code of
+ * licensed works and modifications, which include larger works using a
+ * licensed work, under the same license. Copyright and license notices must
+ * be preserved. Contributors provide an express grant of patent rights.
  */
 
 import fs from 'fs';
